b50dd04d99a709a6f868d8a43a16d4d616f6df46
Bear-a1t <152343852+Bear-a1t@users.noreply.github.com>
2025-08-28 19:27:24 -0400

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,6 +1,6 @@
 import { stateManager } from './state.js';
 import { domElements, domUtils } from './dom.js';
-import { gameManager } from './manager.js';
+import { gameManager } from './game.js';
 import { ANIMATION_DELAYS, CONFIG } from './config.js';
 
 export const uiRenderer = {
