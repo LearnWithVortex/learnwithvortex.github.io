5d85db70ef312e0abcc9607c7fadbfc167ce8a2e
Bear-a1t <152343852+Bear-a1t@users.noreply.github.com>
2025-08-29 08:22:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Auto-bump cache name using build timestamp
-const CACHE_NAME = "vortex-cache-v1.1.1";
+const CACHE_NAME = "vortex-cache-v1";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
