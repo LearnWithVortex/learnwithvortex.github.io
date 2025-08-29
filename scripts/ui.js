7c62acc49789bd413ec165fa6d8e4018cf9f206d
Bear-a1t <152343852+Bear-a1t@users.noreply.github.com>
2025-08-29 08:18:02 -0400

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,6 +1,6 @@
-import { stateManager } from './state-manager.js';
-import { domElements, domUtils } from './dom-elements.js';
-import { gameManager } from './game-manager.js';
+import { stateManager } from './manager.js';
+import { domElements, domUtils } from './dom.js';
+import { gameManager } from './game.js';
 import { ANIMATION_DELAYS, CONFIG } from './config.js';
 
 export const uiRenderer = {
